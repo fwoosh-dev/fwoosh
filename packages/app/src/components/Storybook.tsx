f537ebcf5e293f902479012126f8beffefc10b8f
Andrew Lisowski <lisowski54@gmail.com>
2022-12-30 15:46:23 -0800

    make panel resizable

--- packages/app/src/components/Storybook.tsx
+++ packages/app/src/components/Storybook.tsx
@@ -17,6 +17,7 @@ import { toolbarControls, panels } from "@fwoosh/app/ui";
 import { config } from "@fwoosh/app/config";
 import { Outlet, Link, useParams } from "react-router-dom";
 import { useId } from "@radix-ui/react-id";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import { StoryTree, useStoryTree } from "../hooks/useStoryTree";
 import { ThemeToggle } from "./ThemeToggle";
@@ -43,6 +44,27 @@ const StoryWrapper = styled("div", {
 const PanelContainer = styled("div", {
   height: 400,
   borderTop: "1px solid $gray4",
+});
+
+const PanelResizer = styled("div", {
+  width: "100%",
+  borderTop: "1px solid transparent",
+  zIndex: 100,
+  position: "relative",
+
+  "&:after": {
+    height: 12,
+    position: "absolute",
+    transform: "translateY(-50%)",
+    left: 0,
+    right: 0,
+    top: "50%",
+    content: "''",
+  },
+
+  "&:hover": {
+    borderColor: "$gray10",
+  },
 });
 
 const TreeItem = ({ tree }: { tree: StoryTree }) => {
@@ -75,6 +97,22 @@ const TreeItem = ({ tree }: { tree: StoryTree }) => {
 export const Storybook = () => {
   const tree = useStoryTree();
   const id = useId();
+  const story = (
+    <StoryIdContext.Provider value={id}>
+      <Outlet />
+    </StoryIdContext.Provider>
+  );
+
+  const storyPaneSize = React.useMemo(() => {
+    if (localStorage.getItem("fwoosh:storyPaneSize")) {
+      return Number(localStorage.getItem("fwoosh:storyPaneSize"));
+    }
+
+    return 75;
+  }, []);
+  const storyPaneSizeSet = React.useCallback((size: number) => {
+    localStorage.setItem("fwoosh:storyPaneSize", String(size));
+  }, []);
 
   return (
     <SidebarLayout>
@@ -98,41 +136,57 @@ export const Storybook = () => {
               </Suspense>
             </StoryToolbar>
           )}
-          <StoryIdContext.Provider value={id}>
-            <Outlet />
-          </StoryIdContext.Provider>
-          {panels.length > 0 && (
-            <PanelContainer>
-              <Tabs.Root defaultValue={panels[0]?.componentName}>
-                <Tabs.List>
-                  <Suspense fallback={<Spinner />}>
-                    {panels.map((Panel) => {
-                      return (
-                        <Tabs.Trigger
-                          key={`trigger-${Panel.componentName}`}
+
+          {panels.length > 0 ? (
+            <>
+              <PanelGroup direction="vertical">
+                <Panel
+                  maxSize={75}
+                  defaultSize={storyPaneSize}
+                  onResize={storyPaneSizeSet}
+                >
+                  {story}
+                </Panel>
+                <PanelResizeHandle>
+                  <PanelResizer />
+                </PanelResizeHandle>
+                <Panel maxSize={75}>
+                  <PanelContainer>
+                    <Tabs.Root defaultValue={panels[0]?.componentName}>
+                      <Tabs.List>
+                        <Suspense fallback={<Spinner />}>
+                          {panels.map((Panel) => {
+                            return (
+                              <Tabs.Trigger
+                                key={`trigger-${Panel.componentName}`}
+                                value={Panel.componentName}
+                              >
+                                <Panel.displayName />
+                              </Tabs.Trigger>
+                            );
+                          })}
+                        </Suspense>
+                      </Tabs.List>
+
+                      {panels.map((Panel) => (
+                        <Tabs.Content
+                          key={`content-${Panel.componentName}`}
                           value={Panel.componentName}
                         >
-                          <Panel.displayName />
-                        </Tabs.Trigger>
-                      );
-                    })}
-                  </Suspense>
-                </Tabs.List>
-
-                {panels.map((Panel) => (
-                  <Tabs.Content
-                    key={`content-${Panel.componentName}`}
-                    value={Panel.componentName}
-                  >
-                    <ErrorBoundary>
-                      <Suspense fallback={<Spinner />}>
-                        <Panel storyPreviewId={id} />
-                      </Suspense>
-                    </ErrorBoundary>
-                  </Tabs.Content>
-                ))}
-              </Tabs.Root>
-            </PanelContainer>
+                          <ErrorBoundary>
+                            <Suspense fallback={<Spinner />}>
+                              <Panel storyPreviewId={id} />
+                            </Suspense>
+                          </ErrorBoundary>
+                        </Tabs.Content>
+                      ))}
+                    </Tabs.Root>
+                  </PanelContainer>
+                </Panel>
+              </PanelGroup>
+            </>
+          ) : (
+            story
           )}
         </StoryWrapper>
       </Content>
