5e60728fa6896543d10f7f0c8b8df7d77019bb58
Andrew Lisowski <lisowski54@gmail.com>
2023-01-03 14:05:07 -0800

    organize code

--- packages/app/src/hooks/useStoryTree.ts
+++ packages/app/src/hooks/useStoryTree.ts
@@ -4,83 +4,85 @@ import { StorySidebarChildItem, StoryTree } from "@fwoosh/app/ui";
 import { useQuery } from "react-query";
 import { matchTreeSortingOrder } from "@fwoosh/utils";
 
-export const useStoryTree = () => {
-  const tree = React.useMemo(() => {
-    const treeData: StorySidebarChildItem[] = [];
+function getStories() {
+  const treeData: StorySidebarChildItem[] = [];
 
-    Object.values(stories).forEach((story) => {
-      const { grouping } = story;
-      const levels = grouping.split("/");
-      let currentItem: StorySidebarChildItem | undefined;
+  Object.values(stories).forEach((story) => {
+    const { grouping } = story;
+    const levels = grouping.split("/");
+    let currentItem: StorySidebarChildItem | undefined;
 
-      for (const [index, level] of levels.entries()) {
-        // Set up the root level
+    for (const [index, level] of levels.entries()) {
+      // Set up the root level
+      if (!currentItem) {
+        currentItem = treeData.find((item) => item.name === level);
+
         if (!currentItem) {
-          currentItem = treeData.find((item) => item.name === level);
-
-          if (!currentItem) {
-            currentItem = !("code" in story)
-              ? {
-                  name: level,
-                  id: levels.slice(0, index + 1).join("-"),
-                  mdxFile: story.mdxFile,
-                }
-              : {
-                  name: level,
-                  id: levels.slice(0, index + 1).join("-"),
-                  children: [],
-                };
-            treeData.push(currentItem);
-          }
-
-          continue;
+          currentItem = !("code" in story)
+            ? {
+                name: level,
+                id: levels.slice(0, index + 1).join("-"),
+                mdxFile: story.mdxFile,
+              }
+            : {
+                name: level,
+                id: levels.slice(0, index + 1).join("-"),
+                children: [],
+              };
+          treeData.push(currentItem);
         }
 
-        if ("children" in currentItem) {
-          const childItem = currentItem.children.find(
-            (item) => "name" in item && item.name === level
+        continue;
+      }
+
+      if ("children" in currentItem) {
+        const childItem = currentItem.children.find(
+          (item) => "name" in item && item.name === level
+        );
+
+        if (childItem && "children" in childItem) {
+          currentItem = childItem;
+        } else {
+          const newItem = {
+            name: level,
+            id: levels.slice(0, index + 1).join("-"),
+            children: [],
+          };
+          currentItem.children.push(newItem);
+          currentItem = newItem;
+        }
+
+        // Push the story as a leaf
+        if (index === levels.length - 1 && currentItem) {
+          const folderIndex = currentItem.children.findIndex(
+            (i) => "children" in i
           );
+          const insertIndex =
+            folderIndex > -1 ? folderIndex : currentItem.children.length;
 
-          if (childItem && "children" in childItem) {
-            currentItem = childItem;
+          if ("code" in story) {
+            currentItem.children.splice(insertIndex, 0, {
+              name: story.title,
+              story,
+              id: story.slug,
+            });
           } else {
-            const newItem = {
-              name: level,
-              id: levels.slice(0, index + 1).join("-"),
-              children: [],
-            };
-            currentItem.children.push(newItem);
-            currentItem = newItem;
-          }
-
-          // Push the story as a leaf
-          if (index === levels.length - 1 && currentItem) {
-            const folderIndex = currentItem.children.findIndex(
-              (i) => "children" in i
-            );
-            const insertIndex =
-              folderIndex > -1 ? folderIndex : currentItem.children.length;
-
-            if ("code" in story) {
-              currentItem.children.splice(insertIndex, 0, {
-                name: story.title,
-                story,
-                id: story.slug,
-              });
-            } else {
-              currentItem.children.splice(insertIndex, 0, {
-                name: story.title,
-                mdxFile: story.mdxFile,
-                id: story.slug,
-              });
-            }
+            currentItem.children.splice(insertIndex, 0, {
+              name: story.title,
+              mdxFile: story.mdxFile,
+              id: story.slug,
+            });
           }
         }
       }
-    });
+    }
+  });
 
-    return treeData;
-  }, []);
+  return treeData;
+}
+
+export const useStoryTree = () => {
+  const tree = React.useMemo(getStories, []);
 
   const { data = [] } = useQuery("storyTree", async () => {
     const res = await fetch("/sort", {
