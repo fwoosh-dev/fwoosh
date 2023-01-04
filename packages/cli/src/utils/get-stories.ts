15489fb4833fae5143ca38fb82bd98ef1f96373f
Andrew Lisowski <lisowski54@gmail.com>
2023-01-03 18:01:35 -0800

    improve startup speed by loading stories in parallel

--- packages/cli/src/utils/get-stories.ts
+++ packages/cli/src/utils/get-stories.ts
@@ -46,7 +46,7 @@ export async function convertMarkdownToHtml(markdown: string) {
 
 let parsed = 1;
 
-async function getComment(contents: string, d: { span: { start: number } }) {
+function getComment(contents: string, d: { span: { start: number } }) {
   let i = d.span.start - parsed;
 
   while (contents[i] !== "\n") {
@@ -117,91 +117,95 @@ export interface MDXFileDescriptor {
 
 export type FwooshFileDescriptor = StoryFileDescriptor | MDXFileDescriptor;
 
-export async function getStories({ stories, outDir }: FwooshOptions) {
-  const files = await getStoryList({ stories, outDir });
-  const data: FwooshFileDescriptor[] = [];
+async function getStory(file: string, data: FwooshFileDescriptor[]) {
+  const filename = path.basename(file);
+  const contents = await fs.readFile(file, "utf8");
+  const fullPath = path.resolve(file);
 
-  for (const file of files) {
-    const contents = await fs.readFile(file, "utf8");
-    const fullPath = path.resolve(file);
+  if (file.endsWith(".mdx")) {
+    try {
+      const [, frontmatter] = contents.match(/^---\n(.+)\n---/) || [];
 
-    if (file.endsWith(".mdx")) {
-      try {
-        const [, frontmatter] = contents.match(/^---\n(.+)\n---/) || [];
-
-        if (frontmatter) {
-          data.push({
-            meta: yaml.load(frontmatter) as StoryMeta,
-            mdxFile: fullPath,
-          });
-        }
-      } catch (e) {
-        console.error(e);
+      if (frontmatter) {
+        data.push({
+          meta: yaml.load(frontmatter) as StoryMeta,
+          mdxFile: fullPath,
+        });
       }
-    } else {
-      const ast = await swc.parse(contents, {
-        syntax: "typescript",
-        tsx: true,
-        comments: false,
-        script: true,
-      });
+    } catch (e) {
+      console.error(e);
+    }
+  } else {
+    const ast = await swc.parse(contents, {
+      syntax: "typescript",
+      tsx: true,
+      comments: false,
+      script: true,
+    });
 
-      const exports = ast.body.filter(
-        (node) => node.type === "ExportDeclaration"
-      );
+    const exports = ast.body.filter(
+      (node) => node.type === "ExportDeclaration"
+    );
 
-      const metaDeclaration = exports.find(
-        (e) =>
-          "declaration" in e &&
-          e.declaration.type === "VariableDeclaration" &&
-          "value" in e.declaration.declarations[0].id &&
-          e.declaration.declarations[0].id.value === "meta"
-      );
-      const defaultExport = ast.body.find(
-        (node) => node.type === "ExportDefaultExpression"
-      );
-      const metaObject = metaDeclaration
-        ? (metaDeclaration as any).declaration.declarations[0].init
-        : (defaultExport as any).expression;
-      const meta = metaObject.properties.reduce(
-        (acc: Record<string, unknown>, property: Record<string, any>) => ({
-          ...acc,
-          [property.key.value]:
-            property.key.value === "component"
-              ? getComponentPath(ast, fullPath, property.value.value)
-              : property.value.value,
-        }),
-        {}
-      );
-      const storiesDeclarations = exports.filter((e) => e !== metaDeclaration);
+    const metaDeclaration = exports.find(
+      (e) =>
+        "declaration" in e &&
+        e.declaration.type === "VariableDeclaration" &&
+        "value" in e.declaration.declarations[0].id &&
+        e.declaration.declarations[0].id.value === "meta"
+    );
+    const defaultExport = ast.body.find(
+      (node) => node.type === "ExportDefaultExpression"
+    );
+    const metaObject = metaDeclaration
+      ? (metaDeclaration as any).declaration.declarations[0].init
+      : (defaultExport as any).expression;
+    const meta = metaObject.properties.reduce(
+      (acc: Record<string, unknown>, property: Record<string, any>) => ({
+        ...acc,
+        [property.key.value]:
+          property.key.value === "component"
+            ? getComponentPath(ast, fullPath, property.value.value)
+            : property.value.value,
+      }),
+      {}
+    );
+    const storiesDeclarations = exports.filter((e) => e !== metaDeclaration);
 
-      const stories: Story[] = await Promise.all(
-        (storiesDeclarations as any).map(async (d: any) => {
-          const exportName = d.declaration.declarations[0].id.value;
-          const code = await markdownToHtml.process(
-            endent`
+    const stories: Story[] = await Promise.all(
+      (storiesDeclarations as any).map(async (d: any) => {
+        const exportName = d.declaration.declarations[0].id.value;
+        const code = await markdownToHtml.process(
+          endent`
               \`\`\`tsx
               ${contents.slice(d.span.start - parsed, d.span.end - parsed)}
               \`\`\`
             `
-          );
+        );
 
-          return {
-            exportName,
-            title: capitalCase(exportName),
-            slug: `${paramCase(meta.title)}--${paramCase(exportName)}`,
-            file: fullPath,
-            comment: await getComment(contents, d),
-            code: sanitizeString(String(code)),
-          };
-        })
-      );
+        return {
+          exportName,
+          title: capitalCase(exportName),
+          slug: `${paramCase(meta.title)}--${paramCase(exportName)}`,
+          file: fullPath,
+          comment: getComment(contents, d),
+          code: "",
+          // code: sanitizeString(String(code)),
+        };
+      })
+    );
 
-      data.push({ stories, meta: { ...meta, file: fullPath } });
-      // TODO - this is a hack to get the correct span for the next story
-      parsed = ast.span.end + 2;
-    }
+    data.push({ stories, meta: { ...meta, file: fullPath } });
+    // TODO - this is a hack to get the correct span for the next story
+    parsed = ast.span.end + 2;
   }
+}
+
+export async function getStories({ stories, outDir }: FwooshOptions) {
+  const files = await getStoryList({ stories, outDir });
+  const data: FwooshFileDescriptor[] = [];
+
+  await Promise.all(files.map((file) => getStory(file, data)));
 
   return data;
 }
