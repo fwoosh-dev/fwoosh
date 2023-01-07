10ec8d08801ecf88ab4988874104897949464353
Andrew Lisowski <lisowski54@gmail.com>
2023-01-06 22:05:30 -0800

    delete old docs

--- packages/docs/src/utils/get-docs.ts
+++
@@ -1,8 +0,0 @@
-import { lazy } from "react";
-
-export const modules = Object.fromEntries(
-  Object.entries(import.meta.glob("../pages/*.mdx")).map(([filepath, mod]) => [
-    filepath.replace("../pages/", "").replace(".mdx", ""),
-    lazy(mod as any),
-  ])
-);
