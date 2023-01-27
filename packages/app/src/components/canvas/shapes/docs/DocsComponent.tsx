9798a754e897b194610215e9801ba5560fd3e03e
Andrew Lisowski <lisowski54@gmail.com>
2023-01-26 18:30:58 -0800

    improve canvas perf further

--- packages/app/src/components/canvas/shapes/docs/DocsComponent.tsx
+++ packages/app/src/components/canvas/shapes/docs/DocsComponent.tsx
@@ -11,6 +11,7 @@ import { machine } from "../../machine";
 import { ExternalLink } from "react-feather";
 import { Link } from "react-router-dom";
 import { CanvasMeta } from "../../constants";
+import { StoryData } from "@fwoosh/types";
 
 const ItemWrapper = styled("div", {
   borderRadius: "$round",
@@ -48,53 +49,45 @@ const StoryWrapper = styled("div", {
   width: "max-content",
 });
 
-const flatTree = flattenTree(tree);
+const Story = React.memo(
+  ({
+    item,
+    containerRef,
+    hasBeenMeasured,
+    storyId,
+  }: {
+    item: StoryData;
+    hasBeenMeasured: boolean;
+  } & CanvasMeta) => {
+    const { component: Component, grouping, slug, title } = item;
+    const groups = grouping.split("/");
+    const [measureRef, bounds] = useMeasure();
 
-export const DocsComponent = TLShapeUtil.Component<
-  DocsShape,
-  HTMLDivElement,
-  CanvasMeta
->(({ shape, events, meta }, ref) => {
-  const item = flatTree[shape.id];
-  const [measureRef, bounds] = useMeasure();
-
-  if (!item) {
-    return (
-      <HTMLContainer ref={ref} {...events}>
-        {""}
-      </HTMLContainer>
-    );
-  }
-
-  const { component: Component, grouping, slug, title } = item;
-  const groups = grouping.split("/");
-
-  if (!shape.hasBeenMeasured && bounds.height > 0) {
-    machine.send("UPDATE_DIMENSIONS", {
-      id: shape.id,
-      width: bounds.width,
-      height: bounds.height,
-    });
-
-    if (shape.id === meta.storyId) {
-      machine.send("CENTER_SHAPE", {
-        id: shape.id,
-        client: {
-          height: meta.containerRef.current?.clientHeight,
-          width: meta.containerRef.current?.clientWidth,
-        },
+    if (!hasBeenMeasured && bounds.height > 0) {
+      machine.send("UPDATE_DIMENSIONS", {
+        id: item.slug,
+        width: bounds.width,
+        height: bounds.height,
       });
-    }
-  }
 
-  return (
-    <HTMLContainer ref={ref} {...events}>
+      if (item.slug === storyId) {
+        machine.send("CENTER_SHAPE", {
+          id: item.slug,
+          client: {
+            height: containerRef.current?.clientHeight,
+            width: containerRef.current?.clientWidth,
+          },
+        });
+      }
+    }
+
+    return (
       <React.Suspense fallback={<Spinner delay={1000} />}>
         <ItemWrapper
           ref={measureRef}
           css={{
             boxShadow:
-              meta.storyId === shape.id ? "0 0 0 4px $colors$gray8" : undefined,
+              storyId === item.slug ? "0 0 0 4px $colors$gray8" : undefined,
           }}
         >
           <StoryTitle as={Link} to={`/canvas/workbench/${slug}`}>
@@ -112,6 +105,30 @@ export const DocsComponent = TLShapeUtil.Component<
           </StoryWrapper>
         </ItemWrapper>
       </React.Suspense>
+    );
+  }
+);
+
+const flatTree = flattenTree(tree);
+
+export const DocsComponent = TLShapeUtil.Component<
+  DocsShape,
+  HTMLDivElement,
+  CanvasMeta
+>(({ shape, events, meta }, ref) => {
+  const item = flatTree[shape.id];
+
+  if (!item) {
+    return (
+      <HTMLContainer ref={ref} {...events}>
+        {""}
+      </HTMLContainer>
+    );
+  }
+
+  return (
+    <HTMLContainer ref={ref} {...events}>
+      <Story item={item} {...meta} hasBeenMeasured={shape.hasBeenMeasured} />;
     </HTMLContainer>
   );
 });
