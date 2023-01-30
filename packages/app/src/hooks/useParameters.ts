b7a316d5c47db7b803ba328668a9f461ad8781f6
Andrew Lisowski <lisowski54@gmail.com>
2023-01-30 01:24:52 -0800

    improve ui flashing on nav

--- packages/app/src/hooks/useParameters.ts
+++ packages/app/src/hooks/useParameters.ts
@@ -6,18 +6,22 @@ import { resolveStoryMeta } from "@fwoosh/utils";
 export const useParameters = () => {
   const storyId = useStoryId();
   const story = storyId ? stories[storyId] : undefined;
-  const { data } = useQuery(`params-${storyId}`, async () => {
-    const meta = await resolveStoryMeta(story?.meta);
-    const component =
-      typeof story?.component?._payload?._result === "function"
-        ? await story?.component?._payload?._result()
-        : await story?.component?._payload?._result;
+  const { data } = useQuery(
+    `params-${storyId}`,
+    async () => {
+      const meta = await resolveStoryMeta(story?.meta);
+      const component =
+        typeof story?.component?._payload?._result === "function"
+          ? await story?.component?._payload?._result()
+          : await story?.component?._payload?._result;
 
-    return {
-      ...meta?.parameters,
-      ...component?.default?.parameters,
-    };
-  });
+      return {
+        ...meta?.parameters,
+        ...component?.default?.parameters,
+      };
+    },
+    { suspense: false }
+  );
 
   return data;
 };
